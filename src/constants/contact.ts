915b89e405a425b31492556ed83d1d32773e0f4f
jedlicka-design <milan.jedlicka@lepsi.cz>
2026-01-25 16:33:45 +0100

    Refactoring kontaktů do konstant

---
+++ src/constants/contact.ts
@@ -0,0 +1,7 @@
+export const CONTACT_INFO = {
+    phone: "+420 607 199 629",
+    phonePlain: "+420607199629",
+    email: "musil.jan1@seznam.cz",
+    address: "Drnovice 447, 679 76",
+    owner: "Jan Musil",
+};
